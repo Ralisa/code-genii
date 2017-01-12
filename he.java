6af11cb4a3c83e3b090f5615ba8b0a9a1a916235
Ralisa <helencsea@gmail.com>
2017-01-12 16:22:18 +0530

    Add files via upload

--- he.java
+++ he.java
@@ -1,12 +1,15 @@
+import java.util.*;
+import java.util.Scanner.*;
 class Sample
+
 {
 public static void main(String args[])
 {
-int i=0;
-for(i=0;i<=10;i++)
-{
-System.out.Println("the numbers are"+i);
+int i;
+Scanner s=new Scanner(System.in);
+i=s.nextInt();
+System.out.Println("The number is"+i);
 }
 }
-}
+
 
